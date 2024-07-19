6b45db1e551144ce18a641e9086316b4eb0f1252
Scribe <scribeamanuesis@gmail.com>
2024-07-19 09:09:17 -0700

    Fixed bug with bullet

--- Effects/ShootAt.gd
+++ Effects/ShootAt.gd
@@ -2,7 +2,8 @@ extends Effect
 
 @export var bullet_scene : PackedScene
 @export var bullet_speed : = 1000.0
-#
+@export var collision_exceptions : Array[Node2D]
+
 #func _get_configuration_warnings():
 	#if bullet_scene == null:
 		#return ["You gotta add a bullet to shoot"]
@@ -13,6 +14,8 @@ func activate(target: Object):
 func shoot_at(target : Object):
 	var pos : Vector2 = target.global_position
 	var bullet : RigidBody2D = bullet_scene.instantiate()
+	for node in collision_exceptions:
+		bullet.add_collision_exception(node)
 	add_sibling(bullet)
 	var dir : = global_position.direction_to(pos)
 	bullet.global_position = global_position + (dir * 16)
--- Entities/Bullet.gd
+++ Entities/Bullet.gd
@@ -6,12 +6,13 @@ extends RigidBody2D
 @onready var effects : = $Effects
 
 var already_hit : = []
+var collision_exceptions : = []
 
 func _physics_process(delta):
 	if hit_area.has_overlapping_areas():
 		var areas : Array = hit_area.get_overlapping_areas()
 		for area in areas:
-			if already_hit.has(area):
+			if already_hit.has(area) or collision_exceptions.has(area):
 				continue
 			for effect in effects.get_children():
 				if effect is Effect:
@@ -20,9 +21,6 @@ func _physics_process(delta):
 					pierce -= 1
 					if pierce <= 0:
 						queue_free()
-			#if area is HealthComponent:
-				#area.hit(damage, global_position.direction_to(area.global_position))
-				#pierce -= 1
-				#already_hit.append(area)
-				#if pierce <= 0:
-					#queue_free()
+
+func add_collision_exception(node : Node2D):
+	collision_exceptions.append(node)
