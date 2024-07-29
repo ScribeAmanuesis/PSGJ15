1fa7b33e19bb0ca22b999882dba43301e8a538d0
Matt <mattz3044@outlook.com>
2024-07-29 16:08:50 -0700

    fixed deep frying of day scene and off screen issue of alchemist_desk

--- Scenes/alchemist_desk.gd
+++ Scenes/alchemist_desk.gd
@@ -5,3 +5,7 @@ func _input(event):
 	if event.is_released() and event is InputEventMouseButton:
 		print(event)
 		
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var day: bool = true
-
+@onready var environment = $TD.get_node("WorldEnvironment").environment
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$TD.get_node("WorldEnvironment").environment = null
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,8 +19,10 @@ func _on_next_scene_pressed():
 
 func load_scene(day):
 	if day:
+		$TD.get_node("WorldEnvironment").environment = null
 		$TD.visible = false
 		$AlchemistDesk.visible = true
 	else:
+		$TD.get_node("WorldEnvironment").environment = environment
 		$AlchemistDesk.visible = false
 		$TD.visible = true
