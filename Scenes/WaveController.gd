1c9d0e535aba92d165334f369c7134301b25c626
phenomanonymous <frankiemccormick@gmail.com>
2024-07-30 17:37:17 -0400

    preliminary work on utilizing player shadows to populate td scene

--- Scenes/WaveController.gd
+++ Scenes/WaveController.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
-@export var path : Path2D
+@export var path : Path2D # = get_parent().get_node("PathStart/TDPath")
 
+func _ready():
+	for shadow in Player.shadows:
+		var number = Player.shadows[shadow]
+		var arr = []
+		for i in range(number):
+			arr.append(shadow.instantiate())
+		spawn_wave(arr, Player.wave)
 
 func spawn_wave(enemies : Array, wave : = 1):
 	for enemy in enemies:
-		enemy.health += wave
-		path.add_child(enemy)
+		print_debug(enemy)
+		#enemy.health += wave
+		#path.add_child(enemy)
